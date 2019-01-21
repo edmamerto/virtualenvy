2edfb9559a0e33376013cbac721b5d3695780ee9
Ed Mamerto <edmamerto@gmail.com>
2019-01-21 12:15:47 -0500

    add virtualenvy module

---
+++ virtualenvy/__init__.py
@@ -0,0 +1,31 @@
+
+#!/usr/bin/env python
+
+"""
+@author Ed Mamerto
+"""
+
+import os
+
+
+def create_venv():
+    """
+    creates a venv folder
+    """
+    os.system("virtualenv venv")
+    print """\    
+	        _      _               _                       
+	       (_)    | |             | |                      
+	 __   ___ _ __| |_ _   _  __ _| | ___ _ ____   ___   _ 
+	 \ \ / / | '__| __| | | |/ _` | |/ _ \ '_ \ \ / / | | |
+	  \ V /| | |  | |_| |_| | (_| | |  __/ | | \ V /| |_| |
+	   \_/ |_|_|   \__|\__,_|\__,_|_|\___|_| |_|\_/  \__, |
+	                                                  __/ |
+	                                                 |___/ 
+	"""                                                 
+
+def enter_venv():
+    """
+    enters virtualenvironment
+    """
+    os.system("source venv/bin/activate")
